8625c6754be686d142a3085178464818ed935edc
vpetrash <vasily.petrashkevich@gmail.com>
2021-03-22 22:30:43 -0600

    Added the node_modules and changed config/keys to run via localhost
    1. Added the /node_modules folder into main project folder
    
    2. Changed config/keys to run via localhost

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,10 @@
+//Current version works vial the localhost
+
 module.exports = {
-    MongoURI: 'mongodb+srv://' + process.env.DBLOGIN  + ':' + process.env.DBPASSWORD + '@cluster0.s8jzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://ParadiseTravel:saittravelagency20212021!@cluster0.s8jzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
+
+//Previous version to work via Heroku
+//module.exports = {
+//    MongoURI: 'mongodb+srv://' + process.env.DBLOGIN  + ':' + process.env.DBPASSWORD + '@cluster0.s8jzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//}
